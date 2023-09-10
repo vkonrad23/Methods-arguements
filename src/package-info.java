26edde2b10425011523443fa1e8b797460aca534
vkonrad23 <142899729+vkonrad23@users.noreply.github.com>
2023-09-10 20:51:36 +0300

    Update package-info.java

--- src/package-info.java
+++ src/package-info.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 
 public class Methods {
 
-    // Create a pseudocode for this program. Put the pseudocode into comments at the top of your Main.java
-    // 1. Create a package for the task.
-    // 2. Define a method to print the output.
-    // 3. Define a method to get user input.
-    // 4. Define a method to wrap Y/N dialog.
-    // 5. Break the program into methods.
+
 
     // Define a method to print the output.
     public static void print(String message) {
